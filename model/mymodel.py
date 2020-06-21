b832a47fd0f7e2b8ece188f14bba1a604df3d7fd
Chengkai Yang <jenson.yang20@gmail.com>
2020-06-21 18:26:51 -0400

    fix

--- model/mymodel.py
+++ model/mymodel.py
@@ -13,35 +13,40 @@ class MyModel(torch.nn.Module):
         super(MyModel, self).__init__()
         self.device = device
 
-        self.conv1 = GraphConvolution(in_channels, 2*in_channels)
-        self.conv2 = GraphConvolution(2*in_channels, 2*in_channels)
-        self.conv3 = GraphConvolution(2*in_channels, 2*in_channels)
+        # self.conv1 = GraphConvolution(in_channels, 2*in_channels)
+        # self.conv2 = GraphConvolution(2*in_channels, 2*in_channels)
+        # self.conv3 = GraphConvolution(2*in_channels, 2*in_channels)
 
-        self.lin1 = nn.Linear(2*in_channels, 2*in_channels)
-        self.lin2 = nn.Linear(2*in_channels, in_channels)
+        self.conv1 = GINConv(nn.Sequential(nn.Linear(in_channels, in_channels)
+        , nn.Linear(in_channels, in_channels)))
+        self.conv2 = GINConv(nn.Sequential(nn.Linear(in_channels, in_channels)
+        , nn.Linear(in_channels, in_channels)))
+
+        self.lin1 = nn.Linear(in_channels, in_channels)
+        # self.lin2 = nn.Linear(2*in_channels, in_channels)
         self.lin3 = nn.Linear(in_channels, 1)
 
     def forward(self, data):
-        x = data.x.float()
+        x, edge_index = data.x.float(), data.edge_index.float()
 
         adj = data.adj
 
         
-        adj = torch.FloatTensor(data.adj).view(x.shape[0], x.shape[0])
-        # adj np.inf denote disconnected
-        # adj = adj + 1s
-        adj = F.sigmoid(adj)
-        # adj = torch.where(torch.isnan(adj), torch.zeros_like(adj), adj)
-        adj[torch.isnan(adj)] = 0
-        adj = adj * 4
+        # adj = torch.FloatTensor(data.adj).view(x.shape[0], x.shape[0])
+        # # adj np.inf denote disconnected
+        # # adj = adj + 1s
+        # adj = F.sigmoid(adj)
+        # # adj = torch.where(torch.isnan(adj), torch.zeros_like(adj), adj)
+        # adj[torch.isnan(adj)] = 0
+        # adj = adj * 4
 
-        adj = adj.to(self.device)
+        # adj = adj.to(self.device)
 
-        x = self.conv1(x, adj, self.device)
-        x = self.conv2(x, adj, self.device)
-        x = self.conv3(x, adj, self.device)
+        x = self.conv1(x, edge_index)
+        x = self.conv2(x, edge_index)
+        # x = self.conv3(x, adj, self.device)
         x = F.relu(self.lin1(x))
-        x = F.relu(self.lin2(x))
+        # x = F.relu(self.lin2(x))
         x = self.lin3(x)
         return x
 
@@ -53,7 +58,8 @@ class GraphSAGEModel(torch.nn.Module):
         self.conv1 = GraphConvolutionSage(in_channels, 32)
         self.conv2 = GraphConvolutionSage(32, 32)
 
-        self.lin1 = nn.Linear(32, 1)
+        self.lin1 = nn.Linear(32, 32)
+        self.lin2 = nn.Linear(32, 1)
     
     def reset_parameters(self):
         self.conv1.reset_parameters()
@@ -79,5 +85,6 @@ class GraphSAGEModel(torch.nn.Module):
 
         x = F.relu(self.conv1(x, adj, self.device))
         x = F.relu(self.conv2(x, adj, self.device))
-        x = F.sigmoid(self.lin1(x))
+        x = F.relu(self.lin1(x))
+        x = self.lin2(x)
         return x
