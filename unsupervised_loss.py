d62b352c407e4ce1b95e10595d04ae2c70406fe7
Ziqi Peter <ziqi.zhang@gatech.edu>
2020-06-25 14:03:17 -0400

    incorporate unsupervised part

--- unsupervised_loss.py
+++ unsupervised_loss.py
@@ -1,6 +1,7 @@
 import torch
 import numpy as np
 import random
+import torch.nn.functional as F
 
 class UnsupervisedLoss(object):
     def __init__(self, adj, train_nodes, device):
@@ -10,8 +11,8 @@ class UnsupervisedLoss(object):
         self.WALK_LEN = 1
         self.N_WALK_LEN = 5
         self.MARGIN = 3
-        # eliminate self node
-        self.adj = adj - adj[0,0] * torch.diag(torch.ones((1,adj.shape[0])))
+        
+        self.adj = adj
         self.train_nodes = train_nodes
         self.device = device
 
@@ -45,6 +46,7 @@ class UnsupervisedLoss(object):
                 curr_node = node
                 for j in range(self.WALK_LEN):
                     neighs = np.where(self.adj[curr_node,:] != 0)[0]
+                    # print("random walk:", neighs)
                     next_node = random.choice(neighs)
                     # self co-occurrences are useless
                     if next_node != node and next_node in self.train_nodes:
@@ -54,6 +56,35 @@ class UnsupervisedLoss(object):
             # return all neighbor pairs
             self.node_positive_pairs[node] = cur_pairs
         return self.positive_pairs
+    
+    # negative sampling nodes
+    def get_negtive_nodes(self, nodes, num_neg):
+        """\
+            Conduct negative sampling
+            
+            nodes:
+                The starting nodes of the random walk, training nodes
+            num_neg:
+                Sampling parameters
+        """
+        for node in nodes:
+            # print(node)
+            neighbors = set([node])
+            frontier = set([node])
+            for i in range(self.N_WALK_LEN):
+                current = set()
+                for outer in frontier:
+                    index = set(np.where(self.adj[outer,:] != 0.0)[0])
+                    # print("neg sampling:", index)
+                    current |= index
+                frontier = current - neighbors
+                # print("current: ", current)
+                neighbors |= current
+            far_nodes = set(self.train_nodes) - neighbors
+            neg_samples = random.sample(far_nodes, num_neg) if num_neg < len(far_nodes) else far_nodes
+            self.negtive_pairs.extend([(node, neg_node) for neg_node in neg_samples])
+            self.node_negtive_pairs[node] = [(node, neg_node) for neg_node in neg_samples]
+        return self.negtive_pairs
 
     def extend_nodes(self, nodes, num_neg=6):
         """\
@@ -74,39 +105,15 @@ class UnsupervisedLoss(object):
 
         self.get_negtive_nodes(nodes, num_neg)
         # list of tuple [(node, neigh_i) for node in nodes for neigh_i in neigh(node)]
-        print("positive pairs: ", self.positive_pairs)
-        print("neg pairs: ", self.negtive_pairs)
+        # print("positive pairs: ", len(self.positive_pairs))
+        # print("neg pairs: ", len(self.negtive_pairs))
 
         self.unique_nodes_batch = list(set([i for x in self.positive_pairs for i in x]) | set([i for x in self.negtive_pairs for i in x]))
         # print(set(self.unique_nodes_batch))
-        assert set(self.target_nodes) < set(self.unique_nodes_batch)
+        # print(set(self.target_nodes))
+        assert set(self.target_nodes) <= set(self.unique_nodes_batch)
         return self.unique_nodes_batch
 
-    # negative sampling nodes
-    def get_negtive_nodes(self, nodes, num_neg):
-        """\
-            Conduct negative sampling
-            
-            nodes:
-                The starting nodes of the random walk, training nodes
-            num_neg:
-                Sampling parameters
-        """
-        for node in nodes:
-            neighbors = set([node])
-            frontier = set([node])
-            for i in range(self.N_WALK_LEN):
-                current = set()
-                for outer in frontier:
-                    index = set(np.where(self.adj[int(outer),:] != 0)[0])
-                    current |= index
-                frontier = current - neighbors
-                neighbors |= current
-            far_nodes = set(self.train_nodes) - neighbors
-            neg_samples = random.sample(far_nodes, num_neg) if num_neg < len(far_nodes) else far_nodes
-            self.negtive_pairs.extend([(node, neg_node) for neg_node in neg_samples])
-            self.node_negtive_pairs[node] = [(node, neg_node) for neg_node in neg_samples]
-        return self.negtive_pairs
 
     # normal version of loss
     def get_loss_sage(self, embeddings, nodes):
