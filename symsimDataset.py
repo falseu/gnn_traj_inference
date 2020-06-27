b25d407b26361472c03ad6b6071142cbacb573ba
Ziqi Peter <ziqi.zhang@gatech.edu>
2020-06-26 22:32:55 -0400

    tree

--- symsimDataset.py
+++ symsimDataset.py
@@ -60,7 +60,97 @@ class SymsimDataset(InMemoryDataset):
                                 true_velo = true_velo
                             ))
 
+            scv.pp.filter_and_normalize(adata, min_shared_counts=0, n_top_genes=305)
+            adata = adata[:,:300]
+            print(adata.n_vars)
+
+            # adata = adata[:,:70]
+            # scv.pp.normalize_per_cell(adata)
+            # scv.pp.log1p(adata)
+
+            # compute velocity
+            scv.pp.moments(adata, n_pcs=30, n_neighbors=30)
+            scv.tl.velocity(adata, mode='stochastic')            
+            velo_matrix = adata.layers["velocity"].copy()
+
+            X_spliced = adata.X.toarray()
+
+            pipeline = Pipeline([('pca', PCA(n_components=80, svd_solver='arpack'))])
+            X_pca = pipeline.fit_transform(X_spliced)
+
+            # X_pre = X_spliced + velo_matrix/np.linalg.norm(velo_matrix,axis=1)[:,None]*3
+            X_pre = X_spliced + velo_matrix
+
+            X_pca_pre = pipeline.transform(X_pre)
+            velo_pca = X_pca_pre - X_pca
+
+            directed_conn = kneighbors_graph(X_pca, n_neighbors=10, mode='connectivity', include_self=False).toarray()
+            conn = directed_conn + directed_conn.T
+            conn[conn.nonzero()[0],conn.nonzero()[1]] = 1
+
+            # X_spliced is original, X_pca is after pca
+            x = X_spliced.copy()
+            x = StandardScaler().fit_transform(x)
+            x = torch.FloatTensor(x)
+            # x = torch.FloatTensor(X_pca.copy())
+
+            # X_pca_pre is after pca
+            v = StandardScaler().fit_transform(X_pre)
+            # v = velo_matrix.copy()
+            v = torch.FloatTensor(v)
+
+            # Simulation time label
+            y = X_obs['sim_time'].to_numpy().reshape((-1, 1))
+            scaler = MinMaxScaler((0, 1))
+            scaler.fit(y)
+            y = torch.FloatTensor(scaler.transform(y).reshape(-1, 1))
+
+            # Graph type label
+            # y = torch.LongTensor(np.where(np.array(backbones) == bb)[0])
+
+            edge_index = np.array(np.where(conn == 1))
+            edge_index = torch.LongTensor(edge_index)
+
+            adj = np.full_like(conn, np.nan)
+            for i in range(conn.shape[0]):
+                # self loop
+                adj[i][i] = 0
+
+                indices = conn[i,:].nonzero()[0]
+                for k in indices:
+                    diff = X_pca[i, :] - X_pca[k, :] # 1,d
+                    distance = np.linalg.norm(diff, ord=2) #1
+                    # penalty = np.dot(diff, velo_matrix[k, :, None]) / np.linalg.norm(velo_matrix[k,:], ord=2) / distance
+                    penalty = np.dot(diff, velo_pca[k, :, None]) / np.linalg.norm(velo_pca[k,:], ord=2) / distance
+                    penalty = 0 if np.isnan(penalty) else penalty
+                    adj[i][k] = penalty
+                
+            assert adata.n_vars == 300
+            data = Data(x=x, edge_index=edge_index, y=y, adj=adj, v=v)
+
+            data_list.append(data)
+        
+        # tree
+        tree_files = ['tree/rand'+str(i)+'/' for i in range(1,101)]
+        for file_name in tree_files:
+            X_unspliced = pd.read_csv(root + file_name + "unspliced_counts.txt", sep="\t",header=None).to_numpy().T
+            X_spliced = pd.read_csv(root + file_name + "spliced_counts.txt", sep = "\t",header=None).to_numpy().T
+            true_velo = pd.read_csv(root + file_name + "true_velo.txt", sep="\t",header=None).to_numpy().T
+            true_time = pd.read_csv(root + file_name + "cell_labels.txt", sep = "\t",lineterminator="\n").to_numpy()
+            X_obs = pd.DataFrame(data=true_time, index = ['cell_' + str(x) for x in range(X_unspliced.shape[0])], columns = ['back_bone','sim_time'])
+
+
+            adata = anndata.AnnData(X = csr_matrix(X_spliced),
+                            obs = X_obs,
+                            layers = dict(
+                                unspliced = csr_matrix(X_unspliced),
+                                spliced = csr_matrix(X_spliced),
+                                true_velo = true_velo
+                            ))
+
             scv.pp.filter_and_normalize(adata, min_shared_counts=0, n_top_genes=1000)
+            # adata = adata[:,:300]
+            print(adata.n_vars)
             # adata = adata[:,:70]
             # scv.pp.normalize_per_cell(adata)
             # scv.pp.log1p(adata)
@@ -125,8 +215,8 @@ class SymsimDataset(InMemoryDataset):
             
             data = Data(x=x, edge_index=edge_index, y=y, adj=adj, v=v)
 
+            assert adata.n_vars == 300
             data_list.append(data)
-
         data, slices = self.collate(data_list)
         torch.save((data, slices), self.processed_paths[0])
 
